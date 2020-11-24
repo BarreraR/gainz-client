f804581d638c11b03b2ced69c4fccec705a9b21e
Raul Barrera <rbarrera1992@gmail.com>
2020-11-24 12:33:12 -0600

    more registration styling

--- src/registerMain/RegisterMain.jsx
+++ src/registerMain/RegisterMain.jsx
@@ -20,13 +20,24 @@ export default function RegisterMain() {
         <h2>Welcome</h2>
         <p>Please register to continue</p>
         <label htmlFor='username'>Username </label>
+        <br/>
         <input id='username' type='text' placeholder='user123'/>
         <br/>
         <label htmlFor='password'>Password </label>
+        <br/>
         <input id='password' type='password' placeholder='password'/>
         <br/>
         <label htmlFor='password'>Confirm Password </label>
+        <br/>
         <input id='password' type='password' placeholder='password'/>
+        <br/>
+        <label htmlFor='first_name'>First Name </label>
+        <br/>
+        <input id='first_name' type='text' placeholder='Bob'/>
+        <br/>
+        <label htmlFor='last_name'>Last Name </label>
+        <br/>
+        <input id='last_name' type='text' placeholder='Ross'/>
         <br/>
         <button type='submit'>Enter</button>
       </form>
